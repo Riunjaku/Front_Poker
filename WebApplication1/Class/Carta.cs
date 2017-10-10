68c102019d2cdf1b9f54a539baed64ffaba73125
Eduardo Felipe <eduardo.felipe.s.miranda@gmail.com>
2017-10-10 11:51:10 -0300

    Final

---
+++ WebApplication1/Class/Carta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Class
+{
+    public class Carta
+    {
+        public enum NAIPE
+        {
+            COPAS,
+            ESPADAS,
+            PAUS,
+            OUROS
+        }
+
+        public enum VALOR
+        {
+            DOIS = 2, TRES, QUATRO, CINCO, SEIS, SETE, OITO,
+            NOVE, DEZ, VALETES, RAINHA, REI, AS
+        }
+
+
+        public NAIPE Mnaipe { get; set; }
+        public VALOR Mvalor { get; set; }
+    }
+}
---
+++ WebApplication1/app.js
@@ -0,0 +1,49 @@
+﻿var texasholdemapp = angular.module('texasholdemapp', ['ui.router']);
+
+texasholdemapp.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+            templateUrl: 'partial-home.html'
+        })
+
+        // nested list with custom controller
+        .state('home.poker', {
+            url: '/poker',
+            templateUrl: 'partial-poker.html',
+            controller: function ($scope, $http) {
+
+                $scope.cartasJogador = "";
+                $scope.cartasComputador = "";
+                $scope.ganhador = "";
+                
+                $scope.jogo = jogo;
+
+               
+
+                function jogo() {
+                    $http.get("http://localhost:55038/teste/Jogo")
+                        .then(function (response) {
+                            $scope.cartasJogador = response.data.maoJogadorView;
+                            $scope.cartasComputador = response.data.maoComputadorView;
+                            $scope.ganhador = response.data.ganhador;
+
+                            console.log(response);
+
+
+                        });
+                };
+            }
+        })
+
+
+
+
+
+});
+
